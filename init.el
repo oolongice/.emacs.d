41935fd17c7510ba8d9c8c353907f4845c1c8702
Yu Zhou <yzhoupoipoi@gmail.com>
2026-05-26 17:11:01 -0700

    Add Emacs configuration for package management and UI

---
+++ init.el
@@ -0,0 +1,77 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+;; Basic UI 
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(menu-bar-mode -1)
+(global-display-line-numbers-mode 1)
+(setq inhibit-startup-screen t)
+
+(when (eq system-type 'darwin)
+  (setq dired-use-ls-dired nil))
+
+;; make sure to get the PATH 
+(use-package exec-path-from-shell
+  :ensure t
+  :if (memq window-system '(mac ns x))
+  :config
+  (exec-path-from-shell-initialize))
+
+;; file browser
+(use-package treemacs
+  :bind
+  ("<f8>" . treemacs)
+  :config
+  (setq treemacs-width 30))
+
+;; LSP server
+(use-package company
+  :init (global-company-mode 1)
+  :config
+  (setq company-minimum-prefix-length 1)
+  (setq company-idle-delay 0.1))
+
+(use-package lsp-mode
+  :commands (lsp lsp-deferred)
+  :hook ((c-mode . lsp-deferred)
+         (c++-mode . lsp-deferred))
+  :config 
+  (setq lsp-headerline-breadcrumb-enable t))
+
+(use-package lsp-pyright
+  :ensure t
+  :custom (lsp-pyright-langserver-command "pyright") ;; or basedpyright
+  :hook (python-mode . (lambda ()
+                          (require 'lsp-pyright)
+                          (lsp))))  ; or lsp-deferred
+
+
+;; Reference panel at right side
+(use-package lsp-ui
+  :commands lsp-ui-mode
+  :config
+  (setq lsp-ui-doc-enable t)
+  (setq lsp-ui-doc-position 'at-point)
+  (setq lsp-ui-doc-show-with-cursor t)
+  (setq lsp-ui-imenu-enable t))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
